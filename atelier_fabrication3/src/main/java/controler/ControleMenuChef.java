3b8f53a1485c74a3b5157c410df450ad5413491e
User <User@LAPTOP-QCO3B18E>
2025-05-25 17:16:27 +0200

    Signed-off-by:User <User@172.17.50.211> Signed-off-by:User <User@LAPTOP-QCO3B18E>

--- atelier_fabrication3/src/main/java/controler/ControleMenuChef.java
+++ atelier_fabrication3/src/main/java/controler/ControleMenuChef.java
@@ -27,23 +27,23 @@ public class ControleMenuChef {
     }
     public void afficherProduit (){
         ProduitView nouvelle_vue = new ProduitView();
-        //c'est bizarre que scene ne marche pas, je crois que l'erreur vient de "scene", mais pour le scene est bien définit dans le constructeur de la classe ProduitView
-        nouvelle_vue.actionProd(view.getScene()); //il faudra créer la méthode afficheProduit dans la classe ProduitView mais je n'ai pas osé y toucher pour l'instant
+       
+        nouvelle_vue.actionProd(view.getScene()); 
         }
     
     public void afficherGamme () {
-        GammeView nouvelle_vue = new GammeView(); //bizarre car ca marche ici
-        nouvelle_vue.afficheGamme(view.getScene()); // Pareil je ne sais pas si cette méthode existe, j'adapterai le nom en fonction de ce qui a été mis dans GammeView
+        GammeView nouvelle_vue = new GammeView(); 
+        nouvelle_vue.afficheGamme(view.getScene()); 
         }
     
    public void afficherOperation (){
-       OperationView nouvelle_vue = new OperationView(); //normal que ce ne marche pas, je crois qu'il n'y a pas de constucteur dans la classe OperationView
-       nouvelle_vue.actionOp(view.getScene()); //pareil il faudra que j'aille faire la méthode
+       OperationView nouvelle_vue = new OperationView(); 
+       nouvelle_vue.actionOp(view.getScene()); 
    }
        
    public void afficherEquipement(){
-       EquipementView nouvelle_vue = new EquipementView(); // pas compris : car il y a bien un scene dasn le controleur de EuipementView???
-       nouvelle_vue.afficheequip(view.getScene()); // de meme 
+       EquipementView nouvelle_vue = new EquipementView(); 
+       nouvelle_vue.afficheequip(view.getScene());  
    }
    
    public void afficherPersonnel (){
